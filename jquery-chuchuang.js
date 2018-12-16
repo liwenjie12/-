79cab1695b6baba5df76b525501e5e65bc44f808
liwenjie12 <45912211+liwenjie12@users.noreply.github.com>
2018-12-16 18:01:59 +0800

    Update jquery-chuchuang.js

--- jquery-chuchuang.js
+++ jquery-chuchuang.js
@@ -1,6 +1,7 @@
 
     ;(function () {
         $.fn.lunbo = function () {
+            var flag=0;
             var shunxu = this.find("img");
             console.log(shunxu);
             shunxu.css({
@@ -62,6 +63,9 @@
                 time = setInterval(view, 2000);
             });
             this.children("div").eq(1).on("click",function () {
+                if(flag==1);
+                return;
+                flag=1;
                 shunxu.eq(j-1).fadeIn(1000).siblings("img").fadeOut(500);
                 j--;
                 if(j===-1)
@@ -69,6 +73,9 @@
                 console.log(j);
             });
             this.children("div").eq(0).on("click",function () {
+                if(flag==1);
+                return;
+                flag=1;
                 if(j===shunxu.length-1)
                     j=-1;
                 shunxu.eq(j+1).fadeIn(1000).siblings("img").fadeOut(500);
